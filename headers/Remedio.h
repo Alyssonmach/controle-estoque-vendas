4206e0f247b6d00e71ab70287d12889b70e98ba7
Alyssonmach <alyssonmachado388@gmail.com>
2020-11-13 11:56:39 -0300

    Modificando fluxo da classe Remedio

--- headers/Remedio.h
+++ headers/Remedio.h
@@ -3,6 +3,12 @@
 
 #include "ProdutoPerecivel.h"
 
+struct Drogaria
+{
+	string medida;
+	string info_adicional;
+};
+
 // definindo a classe Remedio (configura e retorna os atributos do remedio)
 // classe deriva de Produto
 class Remedio : public ProdutoPerecivel
@@ -10,23 +16,18 @@ class Remedio : public ProdutoPerecivel
 	// especificador de acesso private
 	private:
 		// membros de dados da classe
-		string medida;
-		string info_adicional;
-		string contraindicacao;
+		Drogaria objeto_remedio;
     // especificador de acesso public
     public:
     	// construtor da classe 
     	Remedio(float = 0.0, float = 0.0, string = " ", string = " ", 
 				string = " ", int = 0, long int = 0, int = 0, int = 0,
-				int = 0, int = 0, int = 0, int = 0, string = " ", string = " ",
-				string = " ", string = " ");
+				int = 0, string = " ", string = " ");
         // membros de funções da classe
+        void set_objeto_remedio(Produto_prototipo, Data, Drogaria);
     	void set_medida(string);
     	void set_info_adicional(string);
-    	void set_contraindicacao(string);
-    	string get_medida(void) const;
-    	string get_info_adicional(void) const;
-    	string get_contraindicacao(void) const;
+    	Drogaria get_objeto_remedio(void) const;
 };
 
 #endif
