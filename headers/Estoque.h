fb153574d8913d487e0878a5ba009fbbfba1fee5
Alyssonmach <alyssonmachado388@gmail.com>
2020-11-13 23:00:49 -0300

    Alterações de progresso

--- headers/Estoque.h
+++ headers/Estoque.h
@@ -21,23 +21,24 @@ class Estoque
     	Estoque();
     	~Estoque();
     	// membros de funções da classe
-    	void set_produto(Produto);
-    	void set_remedio(Remedio);
-    	void set_produto_perecivel(ProdutoPerecivel);
-    	void remover_produto(unsigned int);
-    	void remover_produto_perecivel(unsigned int);
-    	void remover_remedio(unsigned int);
-    	unsigned int pesquisar_produto(long int) const;
-    	unsigned int pesquisar_produto_perecivel(long int) const;
-    	unsigned int pesquisar_remedio(long int) const;
-    	void checar_vencimento_produto_perecivel(int);
-    	void checar_vencimento_remedio(int);
-    	Produto get_produto(unsigned int) const;
-    	ProdutoPerecivel get_produto_perecivel(unsigned int) const;
-    	Remedio get_remedio(unsigned int) const;
-    	vector <Produto> lista_produtos(void) const;
-    	vector <ProdutoPerecivel> lista_pereciveis(void) const;
-    	vector <Remedio> lista_remedios(void) const;
+    	bool salvarlista(void);
+    	bool inserirProduto(Produto);
+    	bool inserirRemedio(Remedio);
+    	bool inserirProdutoPerecivel(ProdutoPerecivel);
+    	//void remover_produto(unsigned int);
+    	//void remover_produto_perecivel(unsigned int);
+    	//void remover_remedio(unsigned int);
+    	//unsigned int pesquisar_produto(long int) const;
+    	//unsigned int pesquisar_produto_perecivel(long int) const;
+    	//unsigned int pesquisar_remedio(long int) const;
+    	//void checar_vencimento_produto_perecivel(int);
+    	//void checar_vencimento_remedio(int);
+    	//Produto get_produto(unsigned int) const;
+    	//ProdutoPerecivel get_produto_perecivel(unsigned int) const;
+    	//Remedio get_remedio(unsigned int) const;
+    	//vector <Produto> lista_produtos(void) const;
+    	//vector <ProdutoPerecivel> lista_pereciveis(void) const;
+    	//vector <Remedio> lista_remedios(void) const;
 };
 
 #endif
