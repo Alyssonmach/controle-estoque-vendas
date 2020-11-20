a417fa652b28d18107cf72001c0e882a0d27e72b
Alyssonmach <alyssonmachado388@gmail.com>
2020-11-20 20:45:29 -0300

    Update Vendas.h

--- headers/Vendas.h
+++ headers/Vendas.h
@@ -9,26 +9,30 @@ class Vendas : public Estoque
 	// Especificado de acesso private
 	private:
 		// membros de dados da classe
-		int entrada_estoque;
-		int saida_estoque;
 		float apurado;
 		float saldo;
+		void calcula_apurado(float);
+    	void calcula_saldo(float);
+    	int conta_notas(void);
+    	string diretorio_nota(void);
+    	vector <Produto> produto_nota;
+    	vector <ProdutoPerecivel> perecivel_nota;
+    	vector <Remedio> remedio_nota;
     // especificador de acesso public
     public:
     	// construtor da classe
     	Vendas();
     	// membros de funções da classe
-    	void monta_nota_fiscal();
-    	void set_entrada_estoque(int);
-    	void set_saida_estoque(int);
-    	void calcula_apurado(float);
-    	void calcula_saldo(float);
-    	int get_estoque_atual(void) const;
-    	int get_saida_de_estoque(void) const;
+    	
+    	void monta_nota_fiscal(void);
     	float get_apurado(void) const;
     	float get_saldo(void) const;
     	float get_despezas(void) const;
-    	float get_lucro(void) const;
+    	void apaga_da_nota_produto(long int);
+    	void apaga_da_nota_perecivel(long int);
+    	void apaga_da_nota_remedio(long int);
+    	void limpa_historico_notas(void);
+    	void restaura_historico_notas(void);
     	void imprime_nota_fiscal(void);
 };
 
