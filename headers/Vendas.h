dcc1cbb215f9aba67e6fae6a64be2f4e3dce07bd
Alyssonmach <alyssonmachado388@gmail.com>
2020-11-20 22:49:52 -0300

    Update Vendas.h

--- headers/Vendas.h
+++ headers/Vendas.h
@@ -3,6 +3,14 @@
 
 #include "Estoque.h"
 
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <dirent.h>
+#include <string>
+
+using namespace std;
+
 // definindo a classe Vendas que herda de Estoque
 class Vendas : public Estoque
 {
@@ -11,29 +19,33 @@ class Vendas : public Estoque
 		// membros de dados da classe
 		float apurado;
 		float saldo;
-		void calcula_apurado(float);
-    	void calcula_saldo(float);
-    	int conta_notas(void);
-    	string diretorio_nota(void);
+		void adiciona_apurado(float);
+    	void adiciona_saldo(float);
+    	int quantidade_notas_fiscais(void);
+    	string diretorio_nota(int);
     	vector <Produto> produto_nota;
     	vector <ProdutoPerecivel> perecivel_nota;
     	vector <Remedio> remedio_nota;
     // especificador de acesso public
     public:
     	// construtor da classe
-    	Vendas();
+    	Vendas(void);
     	// membros de funções da classe
-    	
+    	bool inserir_nota_produto(Produto);
+    	bool inserir_nota_perecivel(ProdutoPerecivel);
+    	bool inserir_nota_remedio(Remedio);
+    	bool apaga_da_nota_produto(long int);
+    	bool apaga_da_nota_perecivel(long int);
+    	bool apaga_da_nota_remedio(long int);
+ 		void imprime_historico_fiscal(void) const;
+    	void limpa_historico_notas(void);
+    	void restaura_historico_notas(void);
+    	void imprime_ultimas_notas(int) const;
+    	string imprimeDataAtual(void) const;
     	void monta_nota_fiscal(void);
     	float get_apurado(void) const;
     	float get_saldo(void) const;
     	float get_despezas(void) const;
-    	void apaga_da_nota_produto(long int);
-    	void apaga_da_nota_perecivel(long int);
-    	void apaga_da_nota_remedio(long int);
-    	void limpa_historico_notas(void);
-    	void restaura_historico_notas(void);
-    	void imprime_nota_fiscal(void);
 };
 
 #endif
