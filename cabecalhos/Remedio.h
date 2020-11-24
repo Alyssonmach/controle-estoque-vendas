628df8dbc0eda78f69f5b7eace4dcaeec440acb9
Alyssonmach <alyssonmachado388@gmail.com>
2020-11-23 21:41:47 -0300

    documentando a classe Remedio

--- cabecalhos/Remedio.h
+++ cabecalhos/Remedio.h
@@ -3,31 +3,29 @@
 
 #include "ProdutoPerecivel.h"
 
+// estrutura que armazena informacoes adicionais associadas a um remedio
 struct Drogaria
 {
 	string medida;
 	string info_adicional;
 };
 
-// definindo a classe Remedio (configura e retorna os atributos do remedio)
-// classe deriva de Produto
 class Remedio : public ProdutoPerecivel
 {
-	// especificador de acesso private
 	private:
-		// membros de dados da classe
+		// atributo que contem uma estrutura com informacoes adicionais do remedio
 		Drogaria objeto_remedio;
-    // especificador de acesso public
     public:
-    	// construtor da classe 
-    	Remedio(float = 0.0, float = 0.0, string = " ", string = " ", 
-				string = " ", int = 0, long int = 0, int = 0, int = 0,
-				int = 0, string = " ", string = " ");
-        // membros de funções da classe
+    	Remedio(float = 0.0, float = 0.0, string = " ", string = " ", string = " ", int = 0, long int = 0, int = 0, int = 0, int = 0, string = " ", string = " ");
+        // metodo que configura um objeto do tipo remedio
         void set_objeto_remedio(Produto_prototipo, Data, Drogaria);
+        // metodo que configura a medida do remedio
     	void set_medida(string);
+    	// metodo que configura informacoes adicionais do remedio
     	void set_info_adicional(string);
+    	// metodo que retorna o objeto do tipo remedio
     	Drogaria get_objeto_remedio(void) const;
+    	// metodo que imprime o objeto remedio
     	void imprimeRemedio(void) const;
 };
 
