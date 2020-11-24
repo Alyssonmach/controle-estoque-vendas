80239bd6f92789e4fa6b31126ac3d9cc4ae511d0
Alyssonmach <alyssonmachado388@gmail.com>
2020-11-23 21:33:22 -0300

    documentando a classe Produto

--- cabecalhos/Produto.h
+++ cabecalhos/Produto.h
@@ -8,7 +8,7 @@ using std::cout;
 using std::endl;
 using std::string;
 
-// definindo uma struct com os atributos do produto
+// atributos associados a um produto basico
 struct Produto_prototipo 
 {
 	float preco_loja;
@@ -20,29 +20,34 @@ struct Produto_prototipo
 	long int codigo;
 };
 
-// definindo a classe Produto (configura e retorna os atributos do produto)
 class Produto 
 {
-	// esécificador de acesso private
 	private:
-		// membro de dados da classe
+		// define uma estrutura com todos os atributos associados a um produto
 		Produto_prototipo objeto_produto;
-    // especificador de acesso public
     public:
-    	// construtor da classe (default)
-    	Produto(float = 0.0, float = 0.0, string = " ", string = " ", string = " ", 
-				int = 0, long int = 0);
-    	// membros de funções da classe
+    	Produto(float = 0.0, float = 0.0, string = " ", string = " ", string = " ", int = 0, long int = 0);
+        // metodo que verifica se um objeto a ser cadastrado e inconsistente
     	bool valida_objeto_produto(Produto_prototipo);
+    	// metodo que configura uma estrutra no objeto da classe produto
 		void set_objeto_produto(Produto_prototipo);
+		// metodo que configura o atributo associado ao preco de aquisicao do produto
 		void set_preco_loja(float);
+		// metodo que configura o atributo associado ao preco repassado ao consumidor
 		void set_preco_consumidor(float);
+		// metodo que configura o atributo associado ao nome do produto
 		void set_nome(string);
+		// metodo que configura o atributo associado a categoria do produto
 		void set_categoria(string);
+		// metodo que configura o atributo associado ao fabricante do produto
 		void set_fabricante(string);
+		// metodo que configura o atributo associado a sua quantidade em estoque
 		void set_quantidade(int);
+		// metodo que configura o atributo associado ao seu codigo de barras
 		void set_codigo(long int);
+		// metodo que retorna uma estrutura com os atributos do objeto
 		Produto_prototipo get_objeto_produto(void) const;
+		// metodo que imprime os atributos do produto para o usuario
 		void imprimeProduto(void) const;
 };
 
