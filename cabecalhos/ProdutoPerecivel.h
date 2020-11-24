a6568aa147dc36ff452523b210f64b5d82745098
Alyssonmach <alyssonmachado388@gmail.com>
2020-11-23 21:38:24 -0300

    documentando a classe ProdutoPerecivel

--- cabecalhos/ProdutoPerecivel.h
+++ cabecalhos/ProdutoPerecivel.h
@@ -3,7 +3,7 @@
 
 #include "Produto.h"
 
-// definindo uma struct com os atributos de data
+// estrutura com os atrbutos da data de vencimento de um produto ou a data atual
 struct Data
 {
 	int dia;
@@ -11,26 +11,26 @@ struct Data
 	int ano;
 };
 
-// definindo a classe ProdutoPerecivel (configura e retorna os atributos do produto perecível)
-// classe herdade de Produto
 class ProdutoPerecivel : public Produto 
 {
-	// especificador de acesso private
 	private:
-	// membros de dados da classe
+		// definindo um atributo para fazer a coleta de data de vencimento do produto e data atual
 		Data data_validade;
     public:
-    	// construtor da clase
-    	ProdutoPerecivel(float = 0.0, float = 0.0, string = " ", string = " ", 
-				string = " ", int = 0, long int = 0, int = 0, int = 0,
-				int = 0);
-    	// membros de funções da classe
-    	void set_objeto_perecivel(Produto_prototipo, Data);
+    	ProdutoPerecivel(float = 0.0, float = 0.0, string = " ", string = " ", string = " ", int = 0, long int = 0, int = 0, int = 0, int = 0);
+    	// metodo que configura um objeto do tipo produto nao perecivel (tem data de validade associada)
+		void set_objeto_perecivel(Produto_prototipo, Data);
+		// metodo que configura a data de validade do produto nao perecivel
     	void set_data_validade(Data);
+    	// metodo que retorna a data de validade do produto nao perecivel
     	Data get_data_validade(void) const;
+    	// metodo que retorna a data atual 
     	Data get_data_atual(void) const;
+    	// metodo que imprime a data atual formatada
     	void imprimeDataAtual(void) const;
+    	// metodo que imprime a data de vencimento do produto formatada
     	void imprimeDataVencimento(void) const;
+    	// metodo que imprime o objeto do tipo produto perecivel 
     	void imprimeProdutoPerecivel(void) const;
 };
 
